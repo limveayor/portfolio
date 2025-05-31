4c52ab68819f964259d9e2a8b43d9afbc9f24242
PrumSokheng <your-email@example.com>
2025-05-31 10:20:24 +0700

    update code navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ export default function Navbar() {
   return (
     <div>
       {/* Navbar */}
-      <nav className=" max-w-8xl mx-auto flex justify-between items-center p-6 shadow-md bg-slate-900 text-white">
-        <div className="text-2xl font-bold">Lim Veayor</div>
-        <ul className="flex space-x-6">
+      <nav className="max-w-8xl mx-auto flex flex-col sm:flex-row justify-between items-center p-4 sm:p-6 shadow-md bg-slate-900 text-white">
+        <div className="text-xl sm:text-2xl font-bold mb-2 sm:mb-0">Lim Veayor</div>
+        <ul className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-6 text-base sm:text-lg md:text-xl">
           <li>
             <a href="#about" className="hover:text-blue-600">
               About
